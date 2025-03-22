dc799cef268c74dab2bab0966b798d2ab8608bff
Victor Sandoval <vs.software.eng@gmail.com>
2025-03-22 16:50:23 +0000

    chore: update 'check' script to use Makefile

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,10 +1,25 @@
 #! /bin/bash
-echo "ruff check --diff fastapi_dramatiq/"
-ruff check --diff fastapi_dramatiq/
+
+echo "make lint"
+make lint DIFF=1
+if [ $? -ne 0 ]; then
+    echo "Error: lint failed"
+    exit 1
+fi
 echo "DONE!"
-echo "ruff format --diff fastapi_dramatiq/"
-ruff format --diff fastapi_dramatiq/
+
+echo "make format"
+make format DIFF=1
+if [ $? -ne 0 ]; then
+    echo "Error: format failed"
+    exit 1
+fi
 echo "DONE!"
-echo "mypy fastapi_dramatiq/"
-mypy fastapi_dramatiq/
+
+echo "make type-check"
+make type-check
+if [ $? -ne 0 ]; then
+    echo "Error: type-check failed"
+    exit 1
+fi
 echo "DONE!"
